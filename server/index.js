994c0e43b5ed61db2232e7c2f1d09490b9ec4276
m-poghosyan <m.poghosyan1996@gmail.com>
2020-06-20 12:11:31 +0400

    some changes

--- server/index.js
+++ server/index.js
@@ -1,15 +1,9 @@
-const path = require('path');
 const dotenv = require('dotenv');
 const express = require('express');
 const app = express();
-const publicPath = path.join(__dirname, '..', 'public');
 const PORT = process.env.PORT || 3000;
 dotenv.config({ path: './config/config.env' });
 
-app.use(express.static(publicPath));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(publicPath, 'index.html'));
-});
 app.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 });
